373b7933e0b622b630dd43602ee5d3d25fb3730e
reinahandal <handalreina@gmail.com>
2023-02-23 22:04:59 +0200

    snackbar alert when color is copied

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react'
 import '../CSS/color.css'
 
-const Color = ({color}) => {
+const Color = ({color, onCopy}) => {
     const [hover, setHover] = useState(false);
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(color);
+        onCopy(color);
+    }
     return (
-        <div className='color-container' style={hover ? {position:'relative', bottom:'10px', boxShadow:"3px 5px 8px lightgray"}: {}} onMouseOver={e=>setHover(true)} onMouseOut={e=>setHover(false)}>
-            <div style={{backgroundColor:color, height:'75%'}}>
-            </div>
-            <div className='color-name'>
-                <h3>{color}</h3>
-            </div>
+        <div 
+        className='color-container' 
+        style={hover ? {backgroundColor: color, position:'relative', bottom:'10px', boxShadow:"0px 5px 8px lightgray"}: {backgroundColor: color}} 
+        onMouseOver={e=>setHover(true)} 
+        onMouseOut={e=>setHover(false)}
+        onClick={handleCopy}
+        >
+            <h3 className='color-name'>{color}</h3>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,22 @@ import Color from './Color';
 import '../CSS/home.css';
 import { useState } from 'react';
 import { Button, Box } from '@mui/material';
+import Snackbar from '@mui/material/Snackbar';
 
 const Home = () => {
     const [colors, setColors] = useState(["#EEEDF0", "#A1B5C1", "#F9ACA7", "#68747D", "#CF365F"]);
-    let randomColors = [];
-
-    const handleSpaceBar = e => {
+    const [alert, setAlert] = useState('');
+    const [open, setOpen] = useState(false);
+    const [duration, setDuration] = useState(4000);
+    
+    onkeydown = (e) => {
         if(e.keyCode === 32) {
             generateColors();
         }
     }
+
     const generateColors = (e) => {
+        let randomColors = [];
         for(let i=0; i<5; i++){
             const randomColor = Math.floor(Math.random()*16777215).toString(16);
             randomColors.push("#".concat(randomColor).toUpperCase());
@@ -21,19 +26,45 @@ const Home = () => {
         setColors(randomColors);
     }
 
+    const copyAlert = (color) => {
+        setAlert(color + ' has been copied to your clipboard');
+        setDuration(4000);
+        setOpen(true);
+    }
+
+    const handleClose = (e, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    }
+
     return (
         <>
+        <Snackbar
+        anchorOrigin={{ vertical:'top', horizontal:'center' }}
+        open={open}
+        onClose={handleClose}
+        message={alert}
+        key={'top' + 'center'}
+        autoHideDuration={duration}
+        ContentProps={{
+            sx: {
+                background: '#FF495C',
+            }
+        }}
+        />
+        <h1 className='header'>COLOR PALETTE GENERATOR</h1>
         <div 
-        className='container' 
-        onKeyDown={handleSpaceBar}
+        className='container'
         tabIndex={0}>
-            <Color color={colors[0]}/>
-            <Color color={colors[1]}/>
-            <Color color={colors[2]}/>
-            <Color color={colors[3]}/>
-            <Color color={colors[4]}/>
+            <Color color={colors[0]} onCopy={copyAlert}/>
+            <Color color={colors[1]} onCopy={copyAlert}/>
+            <Color color={colors[2]} onCopy={copyAlert}/>
+            <Color color={colors[3]} onCopy={copyAlert}/>
+            <Color color={colors[4]} onCopy={copyAlert}/>
         </div> 
-        <Box textAlign='center' sx={{mt:4}}>
+        <Box textAlign='center'>
             <Button 
             variant="contained" 
             style={{backgroundColor:"#B042FF"}} 
